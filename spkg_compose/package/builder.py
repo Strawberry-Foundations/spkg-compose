de8bd328f426bc4da85341523bff8e5d6df9cb55
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-07 21:48:40 +0200

    feat: Added sources

---
+++ spkg_compose/package/builder.py
@@ -0,0 +1,6 @@
+class SpkgPackageBuilder:
+    class Cargo:
+        def __init__(self, data: dict):
+            self.compose_data = data
+
+            self.build_command = data["Build.cargo"]["Exec"]
