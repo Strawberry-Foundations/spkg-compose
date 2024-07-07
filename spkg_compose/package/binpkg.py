8cce91fc61c2980935b35f747eafee8bdaaa59ef
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-07 21:48:37 +0200

    feat: Added sources

---
+++ spkg_compose/package/binpkg.py
@@ -0,0 +1,61 @@
+from spkg_compose import execution_dir
+from binpkg import BinPkg
+from binpkg.metadata import Metadata
+
+import platform
+import os
+
+
+class SpkgBinPkgFormat:
+    def __init__(self, raw_data: dict):
+        self.compose_data = raw_data
+
+        self.prefix = raw_data["Install.binpkg"]["Prefix"]
+        self.target = raw_data["Install.binpkg"]["Target"]
+        self.build_workdir = raw_data["Build"]["Workdir"]
+
+        self.name = self.compose_data["Meta"]["Name"]
+        self.id = self.compose_data["Meta"]["Id"]
+        self.description = self.compose_data["Meta"]["Description"]
+        self.version = self.compose_data["Meta"]["Version"]
+        self.architecture = self.compose_data["Meta"]["Architecture"]
+        self.author = self.compose_data["Meta"]["Author"]
+
+    def makebinpkg(self):
+        os.chdir(f"{execution_dir}/buildpkg")
+        os.mkdir("_binpkg")
+        os.chdir("_binpkg")
+
+        path = ""
+
+        if self.prefix.startswith("/"):
+            path = self.prefix[1:]
+
+        directories = path.split("/")
+
+        current_path = ""
+        for directory in directories:
+            current_path = os.path.join(current_path, directory)
+            if not os.path.exists(current_path):
+                os.makedirs(current_path)
+
+        os.chdir(f"{execution_dir}/buildpkg")
+        os.system(f"cp -r {self.build_workdir}/{self.target} _binpkg/{self.prefix}")
+
+        if self.architecture == "OnBuildSystem":
+            self.architecture = platform.machine()
+
+        BinPkg.create(
+            meta=Metadata(
+                name=self.name,
+                id=self.id,
+                version=self.version,
+                description=self.description,
+                architecture=self.architecture,
+                author=self.author
+            ),
+            source_dir="./_binpkg",
+            output_file=f"{self.id}-{self.version}-{self.architecture}.binpkg"
+        )
+
+        os.system(f"mv {self.id}-{self.version}-{self.architecture}.binpkg ..")
