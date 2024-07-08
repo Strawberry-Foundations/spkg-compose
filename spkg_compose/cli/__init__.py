58e6c13a311ac0d572f53b7d62a28fd26cc89607
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-08 12:26:17 +0200

    feat: cli improvements

--- spkg_compose/cli/__init__.py
+++ spkg_compose/cli/__init__.py
@@ -1,19 +1,25 @@
 from spkg_compose.cli.build import build
+from spkg_compose.cli.args import Args
+from spkg_compose.cli.help import help_cmd
 from spkg_compose.utils.colors import *
 
-from sys import argv
+args = Args()
+
+try:
+    command = args.get(1)
+except IndexError:
+    help_cmd()
+    exit(0)
 
 
-class Args:
-    def __init__(self, cmd_args):
-        self.args = cmd_args
-
-
-args = Args(cmd_args=argv)
-
-match args:
+match args.args[1]:
+    case "help":
+        help_cmd()
     case _:
         if len(args.args) < 2:
             print(f"{BACK_RED}  ERROR  {BACK_RESET}  Missing compose file!")
             exit(1)
-        build(compose_file=args.args[1])
+        try:
+            build(compose_file=args.args[1])
+        except KeyboardInterrupt:
+            print(f"{BACK_YELLOW} WARNING {BACK_RESET}  Canceling operation")
