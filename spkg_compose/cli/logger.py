e2e1b4cd95a49d4a23cb2d705c4581bdf5481014
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-15 22:47:07 +0200

    feat: updated logger

--- spkg_compose/cli/logger.py
+++ spkg_compose/cli/logger.py
@@ -10,9 +10,10 @@ def current_time(time_fmt: str) -> str:
 
 class LogFormat:
     def __init__(self):
-        self.info = f"{C_RESET}{BOLD}[%<time>%] {BLUE}[%<levelname>%]{C_RESET}    [%<message>%]"
+        self.info = f"{C_RESET}{BOLD}[%<time>%] {GREEN}[%<levelname>%]{C_RESET}    [%<message>%]"
+        self.ok = f"{C_RESET}{BOLD}[%<time>%] {BLUE}[%<levelname>%]{C_RESET}      [%<message>%]"
         self.error = f"{C_RESET}{BOLD}[%<time>%] {RED}[%<levelname>%]{C_RESET}   [%<message>%]"
-        self.default = f"{C_RESET}{BOLD}[%<time>%] {BLUE}[%<levelname>%]{C_RESET}   [%<message>%]"
+        self.default = f"{C_RESET}{BOLD}[%<time>%] {CYAN}INIT{C_RESET}   [%<message>%]"
         self.warning = f"{C_RESET}{BOLD}[%<time>%] {YELLOW}[%<levelname>%]{C_RESET} [%<message>%]"
         self.critical = f"{C_RESET}{BOLD}[%<time>%] {YELLOW}[%<levelname>%]{C_RESET} [%<message>%]"
         self.panic = f"{C_RESET}{BOLD}[%<time>%] {RED}[%<levelname>%]{C_RESET}   [%<message>%]"
@@ -26,6 +27,7 @@ class LogLevel(Enum):
     WARNING = 3
     CRITICAL = 4
     PANIC = 5
+    OK = 6
 
 
 class Logger:
@@ -36,6 +38,11 @@ class Logger:
         match level:
             case LogLevel.DEFAULT:
                 return self.formatting.default \
+                    .replace("[%<levelname>%]", level.name) \
+                    .replace("[%<message>%]", content) \
+                    .replace("[%<time>%]", current_time(self.formatting.time_fmt))
+            case LogLevel.OK:
+                return self.formatting.ok \
                     .replace("[%<levelname>%]", level.name) \
                     .replace("[%<message>%]", content) \
                     .replace("[%<time>%]", current_time(self.formatting.time_fmt))
@@ -67,6 +74,9 @@ class Logger:
 
     def default(self, log_message: str):
         print(self.parse(LogLevel.DEFAULT, log_message))
+
+    def ok(self, log_message: str):
+        print(self.parse(LogLevel.OK, log_message))
 
     def info(self, log_message: str):
         print(self.parse(LogLevel.INFO, log_message))
