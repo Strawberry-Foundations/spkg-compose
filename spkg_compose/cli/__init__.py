f8b3c7086ba6475069b5ace035d734b41b0a80af
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-15 21:56:01 +0200

    fix: fixed logger bugs

--- spkg_compose/cli/__init__.py
+++ spkg_compose/cli/__init__.py
@@ -1,11 +1,11 @@
 from spkg_compose.cli.build import build
 from spkg_compose.cli.args import Args
 from spkg_compose.cli.help import help_cmd
-from spkg_compose.cli.logger import Logger
+from spkg_compose.cli.logger import logger
+from spkg_compose.server import server_main
 from spkg_compose.utils.colors import *
 
 args = Args()
-logger = Logger()
 
 try:
     command = args.get(1)
@@ -17,6 +17,8 @@ except IndexError:
 match args.args[1]:
     case "help":
         help_cmd()
+    case "server":
+        server_main()
     case "build":
         if len(args.args) < 3:
             print(f"{BACK_RED}  ERROR  {BACK_RESET}  Missing compose file!")
