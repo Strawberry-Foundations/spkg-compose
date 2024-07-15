7dfeefee6d19d30cbb545d11b632e4b3cf1f2a7b
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-16 00:23:58 +0200

    feat: updated server

--- spkg_compose/server/git.py
+++ spkg_compose/server/git.py
@@ -6,7 +6,6 @@ import requests
 
 
 def fetch_git(server):
-    print("Fetching git releases...")
     for root, _, files in os.walk(server.config.data_dir):
         for file in files:
             if file.endswith('.spkg'):
@@ -14,27 +13,47 @@ def fetch_git(server):
 
                 data = read(file_path)
                 package = SpkgBuild(data)
+
                 repo_url = package.meta.source
+
                 if repo_url.startswith("https://github.com"):
-                    check_for_new_release(repo_url)
+                    check_for_new_release_or_commit(repo_url)
 
 
-def check_for_new_release(repo_url):
-    api_url = convert_to_github_api_url(repo_url)
+def check_for_new_release_or_commit(repo_url):
+    api_url = convert_to_github_api_url(repo_url, 'releases')
     headers = {'Accept': 'application/vnd.github.v3+json'}
     response = requests.get(api_url, headers=headers)
+
     if response.status_code == 200:
         releases = response.json()
         if releases:
             latest_release = releases[0]
             print(f"New release found for {repo_url}: {latest_release['tag_name']}")
+        else:
+            print(f"No releases found for {repo_url}. Checking latest commit.")
+            check_latest_commit(repo_url)
     else:
         print(f"Failed to fetch releases for {repo_url}")
 
 
-def convert_to_github_api_url(repo_url):
+def check_latest_commit(repo_url):
+    api_url = convert_to_github_api_url(repo_url, 'commits')
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        commits = response.json()
+        if commits:
+            latest_commit = commits[0]
+            print(f"Latest commit for {repo_url}: {latest_commit['sha']}")
+    else:
+        print(f"Failed to fetch commits for {repo_url}")
+
+
+def convert_to_github_api_url(repo_url, endpoint):
     parts = repo_url.rstrip('/').split('/')
     owner = parts[-2]
     repo = parts[-1]
-    api_url = f"https://api.github.com/repos/{owner}/{repo}/releases"
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/{endpoint}"
     return api_url
