ac093c5fa1802f1d45f8605c4870dfa882951104
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-15 22:27:38 +0200

    feat: added config

---
+++ spkg_compose/server/config.py
@@ -0,0 +1,14 @@
+from spkg_compose import init_dir
+
+import yaml
+
+with open(init_dir + "/data/config.yml", "r") as _config:
+    config_data = yaml.load(_config, Loader=yaml.SafeLoader)
+
+
+class Config:
+    def __init__(self):
+        self.data_dir = config_data["server"]["data_dir"]
+
+
+config = Config()
