929b5f5b704ec6c27cab9de6eb501f9b2a8885ad
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-15 22:17:46 +0200

    feat: added init_dir

--- spkg_compose/__init__.py
+++ spkg_compose/__init__.py
@@ -2,6 +2,7 @@ import os
 
 execution_dir = os.getcwd()
 server_dir = os.path.dirname(os.path.realpath(__file__))
+init_dir = os.path.dirname(server_dir)
 
 VERSION = "1.1.0"
 SERVER_VERSION = "1.0.0"
