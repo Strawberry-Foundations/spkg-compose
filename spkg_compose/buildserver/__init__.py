693a9779908d8b4f69d114e483080dec5a64e9a7
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-16 15:27:55 +0200

    fix: fixed imports

--- spkg_compose/buildserver/__init__.py
+++ spkg_compose/buildserver/__init__.py
@@ -1,21 +1,6 @@
-import sys
-
 from spkg_compose import SERVER_VERSION, init_dir
-from spkg_compose.server.config import config as _cfg
-from spkg_compose.server.git import fetch_git
-from spkg_compose.core.parser import read
-from spkg_compose.cli.logger import logger, current_time
-from spkg_compose.utils.colors import *
-from spkg_compose.utils.time import unix_to_readable
-from spkg_compose.package import SpkgBuild
-
-from datetime import datetime, timedelta
-
-import os
-import json
-import time
-import threading
-import requests
+from spkg_compose.buildserver import config as _cfg
+from spkg_compose.cli.logger import logger
 
 
 class BuildServer:
@@ -25,7 +10,6 @@ class BuildServer:
         self.index = f"{init_dir}/data/index.json"
 
     def run(self):
-
         pass
 
 
