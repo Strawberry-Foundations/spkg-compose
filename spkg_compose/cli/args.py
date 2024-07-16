41439eec8a761cdf6e79bdbca0a5ed90583a4b4e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-16 13:41:50 +0200

    feat: added parse_args()

--- spkg_compose/cli/args.py
+++ spkg_compose/cli/args.py
@@ -4,6 +4,21 @@ from sys import argv
 class Args:
     def __init__(self):
         self.args = argv
+        self.options = self.parse_args()
 
     def get(self, index: int):
         return self.args[index]
+
+    def parse_args(self):
+        arguments = {}
+        i = 0
+        while i < len(self.args):
+            if self.args[i].startswith('--'):
+                key = self.args[i].lstrip('-')
+                if i + 1 < len(self.args) and not self.args[i + 1].startswith('--'):
+                    arguments[key] = self.args[i + 1]
+                    i += 1
+                else:
+                    arguments[key] = None
+            i += 1
+        return arguments
