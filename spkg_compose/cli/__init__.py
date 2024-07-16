f6f0c227be17daac0113389409b6e3da3cd052c2
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-16 15:20:48 +0200

    feat: Added build server

--- spkg_compose/cli/__init__.py
+++ spkg_compose/cli/__init__.py
@@ -3,6 +3,7 @@ from spkg_compose.cli.args import Args
 from spkg_compose.cli.help import help_cmd
 from spkg_compose.cli.logger import logger
 from spkg_compose.server import server_main
+from spkg_compose.server.buildserver import build_server_main
 from spkg_compose.utils.colors import *
 
 args = Args()
@@ -20,6 +21,8 @@ match args.args[1]:
         help_cmd()
     case "server":
         server_main(args.index_start(2))
+    case "build-server":
+        build_server_main(args.index_start(2))
     case "build":
         if len(args.args) < 3:
             print(f"{BACK_RED}  ERROR  {BACK_RESET}  Missing compose file!")
