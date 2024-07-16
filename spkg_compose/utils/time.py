353c8acf13f6b5f51f65304c209340038073e500
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-16 21:21:38 +0200

    feat: added convert_time()

--- spkg_compose/utils/time.py
+++ spkg_compose/utils/time.py
@@ -8,15 +8,15 @@ def unix_to_readable(unix_time):
 
 def convert_time(elapsed_time):
     if elapsed_time < 1:
-        time_str = f"{elapsed_time * 1000:.2f} ms"
+        time_str = f"{elapsed_time * 1000:.2f}ms"
     elif elapsed_time < 60:
-        time_str = f"{elapsed_time:.2f} s"
+        time_str = f"{elapsed_time:.2f}s"
     elif elapsed_time < 3600:
         minutes, seconds = divmod(elapsed_time, 60)
-        time_str = f"{int(minutes)} m {seconds:.2f} s"
+        time_str = f"{int(minutes)} m {seconds:.2f}s"
     else:
         hours, remainder = divmod(elapsed_time, 3600)
         minutes, seconds = divmod(remainder, 60)
-        time_str = f"{int(hours)} h {int(minutes)} m {seconds:.2f} s"
+        time_str = f"{int(hours)}h {int(minutes)}m {seconds:.2f}s"
 
     return time_str
