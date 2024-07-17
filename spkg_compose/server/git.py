170ddbc83fabe6e8e785d46ef7ed23d683fba312
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-17 13:42:34 +0200

    feat: added rt_logger as parameter

--- spkg_compose/server/git.py
+++ spkg_compose/server/git.py
@@ -1,11 +1,12 @@
 from spkg_compose.core.parser import read
 from spkg_compose.server.api.github import GitHubApi
 from spkg_compose.package import SpkgBuild
+from spkg_compose.cli.logger import RtLogger
 
 import os
 
 
-def fetch_git(server):
+def fetch_git(server, rt_logger: RtLogger):
     for root, _, files in os.walk(server.config.data_dir):
         for file in files:
             if file.endswith('.spkg'):
