09daa69df51dedd8c644de7ce1e00e6c2c99850e
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-19 15:44:37 +0200

    bump: updated version

--- spkg_compose/__init__.py
+++ spkg_compose/__init__.py
@@ -5,5 +5,5 @@ server_dir = os.path.dirname(os.path.realpath(__file__))
 init_dir = os.path.dirname(server_dir)
 
 VERSION = "1.2.2"
-SERVER_VERSION = "1.2.13"
+SERVER_VERSION = "1.2.14"
 BUILD_SERVER_VERSION = "1.1.7"
