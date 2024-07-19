c87274e4f67197d628c6e78176896df70dd417b9
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-19 19:02:02 +0200

    feat: add utils.path

--- spkg_compose/utils/path.py
+++ spkg_compose/utils/path.py
@@ -1,4 +1,4 @@
 def extract_path(url):
     trimmed_url = url.split('/main')[1]
-    path_without_filename = "/".join(trimmed_url.split('/')[:-1])
-    return path_without_filename
+    path_with_main = "/main" + "/".join(trimmed_url.split('/')[:-1])
+    return path_with_main
