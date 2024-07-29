eb6615a3221382608b14b938f1cb0dd5d39d7661
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-29 23:39:20 +0200

    fat: add extract_base_url & extract_base_path

--- spkg_compose/utils/path.py
+++ spkg_compose/utils/path.py
@@ -2,3 +2,13 @@ def extract_path(url):
     trimmed_url = url.split('/packages')[1]
     path_without_filename = "/".join(trimmed_url.split('/')[:-1])
     return path_without_filename
+
+
+def extract_base_url(url):
+    trimmed_url = url.split('/main')[0]
+    return trimmed_url
+
+
+def extract_base_path(url):
+    trimmed_url = "/".join(url.split('/')[:-1])
+    return trimmed_url
