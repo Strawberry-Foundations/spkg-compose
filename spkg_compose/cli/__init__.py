28a5fa4266096b5e84a4a426b4ef6911eacb272b
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-29 15:51:05 +0200

    fix: fix imports

--- spkg_compose/cli/__init__.py
+++ spkg_compose/cli/__init__.py
@@ -2,9 +2,6 @@ from spkg_compose.cli.build import build
 from spkg_compose.cli.args import Args
 from spkg_compose.cli.help import help_cmd
 from spkg_compose.cli.logger import logger
-from spkg_compose.server import server_main
-from spkg_compose.buildserver import build_server_main
-from spkg_compose.http.repo import repo_api_main
 from spkg_compose.utils.colors import *
 
 args = Args()
@@ -22,12 +19,15 @@ match args.args[1]:
         help_cmd()
 
     case "server":
+        from spkg_compose.server import server_main
         server_main(args.index_start(2))
 
     case "build-server":
+        from spkg_compose.buildserver import build_server_main
         build_server_main(args.index_start(2))
 
     case "repo-api":
+        from spkg_compose.http.repo import repo_api_main
         repo_api_main()
 
     case "build":
