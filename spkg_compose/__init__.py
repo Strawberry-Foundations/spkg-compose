5ec4da75700643bf7233d2171ca92f24de41d6af
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-14 13:55:59 +0200

    chore: update version

--- spkg_compose/__init__.py
+++ spkg_compose/__init__.py
@@ -4,6 +4,6 @@ execution_dir = os.getcwd()
 server_dir = os.path.dirname(os.path.realpath(__file__))
 init_dir = os.path.dirname(server_dir)
 
-VERSION = "1.2.4"
+VERSION = "1.2.5"
 SERVER_VERSION = "1.2.22"
-BUILD_SERVER_VERSION = "1.1.16"
+BUILD_SERVER_VERSION = "1.1.17"
